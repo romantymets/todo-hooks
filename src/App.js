bb887029e9a992f449210d7d0339184e2f8d2a93
romantymets <romantymets@gmail.com>
2021-02-07 21:40:11 +0200

    new some page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Todo from "./containers/Todo/Todo"
+import TodoContainers from "./containers/Todo/TodoContainer"
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
-    <div className="container-fluid">
-    <Todo/>
+    <div className="container">
+    <TodoContainers/>
     </div>
   );
 }
