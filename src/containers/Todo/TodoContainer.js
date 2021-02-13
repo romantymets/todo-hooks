b539344134d7800365aeb0d166abcaa034ee4480
romantymets <romantymets@gmail.com>
2021-02-13 13:44:45 +0200

    added new list to the site

--- src/containers/Todo/TodoContainer.js
+++ src/containers/Todo/TodoContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import List from "./component/List/List";
 import Api from "../../api/Api";
 
@@ -6,14 +6,52 @@ function TodoContainer() {
   const [InputText, setInputText] = useState("");
   const [todo, setTodo] = useState([]);
 
+  const onAddTodo = (e) => {
+    e.preventDefault();
+    const inputText = InputText;
+    const newTodo = todo;
+    Api.post("/todo", {
+      title: inputText
+    }
+      )
+      .then((response) => {
+        const { data } = response;
+        setTodo([...newTodo, data])
+      })
+      .catch((error) => {
+        alert(error.message)
+      })
+  } ;
   function onInput (e) {
   const inputtext = e.target.value;
   setInputText( inputtext )
   }
+  useEffect(() => {
+    Api.get("/todo")
+      .then((response) => {
+        const { data } = response;
+        setTodo( data )
+      })
+      .catch((error) => {
+        alert(error.message)
+      })
+  }, []);
+  const deleteTodo = (_id) => {
+    const findIndexElement = todo.findIndex(todo => todo._id === _id);
+    Api.remove(`/todo/${_id}`)
+      .then(() => {
+        const newTodo = [...todo];
+        newTodo.splice(findIndexElement,1);
+        setTodo(newTodo)
+      })
+      .catch((error) => {
+        alert(error.message)
+      })
+  };
 
   return (
     <div className='container'>
-      <form className='container'>
+      <form className='container' onSubmit={onAddTodo}>
         <div className="form-group">
           <h2> Enter Todo </h2>
           <input type="text" className="form-control"
@@ -22,7 +60,10 @@ function TodoContainer() {
         <button type="submit" className="btn btn-primary"> Submit </button>
         </div>
       </form>
-      <List/>
+      <List
+        todos = {todo}
+        deleteTodo={deleteTodo}
+      />
     </div>
   );
 }
--- src/containers/Todo/component/List/List.js
+++ src/containers/Todo/component/List/List.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
 
-function List () {
+function List ({ todos, deleteTodo }) {
   return (
-    <div className='container'>
-
-    </div>
+    <ul className="list-group list-group-flush">
+      {todos.map(
+        (todo, index) => (
+          <li className="list-group-item" key={todo._id}>
+            <div className="row">
+              <div className="col-2"> {index+1}</div>
+              <div className="col-2"> {index+1}</div>
+              <div className="col-6"> {todo.title}</div>
+               <div className="col-4">
+                 <button className="btn-danger" onClick={() => {deleteTodo(todo._id)}}> X </button>
+               </div>
+            </div>
+          </li>
+        )
+      )}
+    </ul>
   );
 }
 
